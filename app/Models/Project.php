a69585fe7ed3bf88fdb7fe830bbe9e844b63dca9
MarcoMazzilli <mazzillimarco97@gmail.com>
2023-06-22 16:48:04 +0200

    [Add] Create function bolongsToMany on `Models`

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -40,4 +40,8 @@ class Project extends Model
     public function type(){
         return $this->belongsTo(Type::class);
     }
+
+    public function technologies(){
+        return $this->belongsToMany('App\Models\Technology');
+    }
 }
