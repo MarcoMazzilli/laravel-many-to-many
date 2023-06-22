a58e961add0cf7c05142f5f6bce6f0b54c8a8382
MarcoMazzilli <mazzillimarco97@gmail.com>
2023-06-22 19:59:13 +0200

    [Add] condition to update on ProjectController

--- app/Http/Controllers/admin/ProjectController.php
+++ app/Http/Controllers/admin/ProjectController.php
@@ -103,6 +103,12 @@ class ProjectController extends Controller
 
         $project->update($form_data);
 
+        if (array_key_exists('technologies', $form_data)) {
+            $project->technologies()->sync($form_data['technologies']);
+        }else{
+            $project->technologies()->detach();
+        }
+
         return redirect()->route('admin.project.show',compact('project'));
     }
 
